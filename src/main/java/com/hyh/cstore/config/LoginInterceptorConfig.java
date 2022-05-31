239c0ca26dee48d37c247ff87aeea7e6634a6035
Yuzu <qj17687313829@outlook.com>
2022-05-31 18:56:24 +0800

    拦截器改动,爆捏肉肉

--- src/main/java/com/hyh/cstore/config/LoginInterceptorConfig.java
+++ src/main/java/com/hyh/cstore/config/LoginInterceptorConfig.java
@@ -1,33 +1,34 @@
 package com.hyh.cstore.config;
 
 import com.hyh.cstore.interceptor.LoginInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import java.util.ArrayList;
 @Configuration
 public class LoginInterceptorConfig implements WebMvcConfigurer {
+
+
+    @Autowired
+    private LoginInterceptor loginInterceptor;
+
+
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        HandlerInterceptor loginInterceptor = new LoginInterceptor();
-        ArrayList<String> patterns = new ArrayList<>();
-        patterns.add("/bootstraps3/**");
-        patterns.add("/css/**");
-        patterns.add("/images/**");
-        patterns.add("/js/**");
-        patterns.add("/web/login.html");
-        patterns.add("/web/index.html");
-        patterns.add("/web/product.html");
-        patterns.add("/web/users.html");
-        patterns.add("/users/login");
-        patterns.add("/users/reg");
-        patterns.add("/districts/**");
-        patterns.add("/products/**");
-//        registry.addInterceptor(loginInterceptor)
-//                .addPathPatterns("/**")
-//                .excludePathPatterns(patterns);
+        registry.addInterceptor(loginInterceptor)
+                .addPathPatterns("/**")
+                .excludePathPatterns(
+                        "/**/*.css",
+                        "/**/*.js",
+                        "/**/*.jpg",
+                        "/**/*.png",
+                        "/web/login.html",
+                        "/web/register.html",
+                        "/users/login",
+                        "/users/reg",
+                        "/districts/**",
+                        "/products/**"
+                        );
     }
 }
--- src/main/java/com/hyh/cstore/interceptor/LoginInterceptor.java
+++ src/main/java/com/hyh/cstore/interceptor/LoginInterceptor.java
@@ -1,11 +1,13 @@
 package com.hyh.cstore.interceptor;
 
+import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+@Component
 public class LoginInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
